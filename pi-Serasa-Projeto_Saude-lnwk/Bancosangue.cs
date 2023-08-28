618ad26c72d3856995e2dd174d6cd8543c7968d6
NATANAEL ANACLETO DE PAULA <natanael.apaula@senacsp.edu.br>
2023-08-28 16:02:25 -0300

    Fiz o banco de sangue

---
+++ pi-Serasa-Projeto_Saude-lnwk/Bancosangue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pi_Serasa_Projeto_Saude_lnwk
+{
+    public partial class Bancosangue : Form
+    {
+
+        private Panel panel;
+        public Bancosangue()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            panel = new Panel();
+            panel.BackColor = System.Drawing.Color.Red;
+            panel.Dock = DockStyle.Top;
+            panel.Height = 100;
+
+            Controls.Add(panel);
+
+            KeyDown += MainForm_KeyDown;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F11)
+            {
+                ToggleFullScreenMode();
+            }
+        }
+
+        private void ToggleFullScreenMode()
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                WindowState = FormWindowState.Normal;
+                panel.Dock = DockStyle.Top;
+            }
+            else
+            {
+                WindowState = FormWindowState.Maximized;
+                panel.Dock = DockStyle.None;
+                panel.Height = 0;
+            }
+        }
+    }
+
+
+
+
+
+}
+
--- pi-Serasa-Projeto_Saude-lnwk/Form1.Designer.cs
+++ pi-Serasa-Projeto_Saude-lnwk/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 137);
+            this.button1.Location = new System.Drawing.Point(497, 243);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(123, 58);
             this.button1.TabIndex = 0;
