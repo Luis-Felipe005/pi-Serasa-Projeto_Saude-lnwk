80d60509ec58c1fe1a9e0a43317145fc100e6d9e
WILLIAM DRESCH <william.dresch@senacsp.edu.br>
2023-08-29 15:07:55 -0300

    Recepcao Concluido

---
+++ pi-Serasa-Projeto_Saude-lnwk/RecepcaoAtendente.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace pi_Serasa_Projeto_Saude_lnwk
+{
+    partial class RecepcaoAtendente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RecepcaoAtendente));
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            listView1 = new ListView();
+            label2 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ControlLightLight;
+            label1.Location = new Point(209, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(360, 37);
+            label1.TabIndex = 0;
+            label1.Text = "RECEPÇÃO DO ATENDENTE";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(304, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(93, 44);
+            button1.TabIndex = 1;
+            button1.Text = "ACEITAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(421, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(93, 44);
+            button2.TabIndex = 2;
+            button2.Text = "RECUSAR";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(509, 66);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(303, 225);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(listView1);
+            panel1.Location = new Point(12, 66);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(296, 247);
+            panel1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(0, 61);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(296, 183);
+            listView1.TabIndex = 7;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(35, 15);
+            label2.Name = "label2";
+            label2.Size = new Size(234, 25);
+            label2.TabIndex = 8;
+            label2.Text = "Informações Do Paciente";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Gray;
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(0, 319);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(824, 55);
+            panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Black;
+            panel3.Controls.Add(label1);
+            panel3.Location = new Point(0, 6);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(824, 40);
+            panel3.TabIndex = 5;
+            // 
+            // RecepcaoAtendente
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(824, 371);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            Name = "RecepcaoAtendente";
+            Text = "RecepcaoAtendente";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private ListView listView1;
+        private Label label2;
+        private Panel panel2;
+        private Panel panel3;
+    }
+}
