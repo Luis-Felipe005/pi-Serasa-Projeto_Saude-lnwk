ee6803939b81653354958bee8486095c99984298
NATANAEL ANACLETO DE PAULA <natanael.apaula@senacsp.edu.br>
2023-08-29 15:05:02 -0300

    Tentativa 3

--- pi-Serasa-Projeto_Saude-lnwk/ColetaExame.Designer.cs
+++ pi-Serasa-Projeto_Saude-lnwk/ColetaExame.Designer.cs
@@ -51,9 +51,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-34, 338);
+            this.pictureBox1.Location = new System.Drawing.Point(-34, 292);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(844, 113);
+            this.pictureBox1.Size = new System.Drawing.Size(844, 159);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
