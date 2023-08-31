d804b5259033a28c56ba2ec880b6baf9576ec571
williamDresch016 <135863277+williamDresch016@users.noreply.github.com>
2023-08-31 16:34:45 -0300

    Centralizacao Feita

--- pi-Serasa-Projeto_Saude-lnwk/Form2.Designer.cs
+++ pi-Serasa-Projeto_Saude-lnwk/Form2.Designer.cs
@@ -28,123 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(166, 119);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(185, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Doação de sangue";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(166, 252);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(185, 34);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Coleta de exame";
-            this.button2.UseVisualStyleBackColor = true;
+            button3 = new Button();
+            lblOpcoes = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            painelMeio = new Panel();
+            button5 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            panel1.SuspendLayout();
+            painelMeio.SuspendLayout();
+            SuspendLayout();
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(502, 119);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(178, 39);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Vacinações";
-            this.button3.UseVisualStyleBackColor = true;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.Location = new Point(213, 10);
+            button3.Name = "button3";
+            button3.Size = new Size(178, 39);
+            button3.TabIndex = 2;
+            button3.Text = "Vacinações";
+            button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // lblOpcoes
             // 
-            this.button4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button4.Location = new System.Drawing.Point(502, 250);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(178, 36);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Consultas";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button5.Location = new System.Drawing.Point(380, 310);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(88, 30);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "Cancelar";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(317, 38);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(210, 21);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "VER OPÇÕES DE SERVIÇOS";
+            lblOpcoes.AutoSize = true;
+            lblOpcoes.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            lblOpcoes.Location = new Point(317, 38);
+            lblOpcoes.Name = "lblOpcoes";
+            lblOpcoes.Size = new Size(210, 21);
+            lblOpcoes.TabIndex = 5;
+            lblOpcoes.Text = "VER OPÇÕES DE SERVIÇOS";
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(172)))), ((int)(((byte)(255)))));
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(-2, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(804, 100);
-            this.panel1.TabIndex = 6;
+            panel1.BackColor = Color.FromArgb(139, 172, 255);
+            panel1.Controls.Add(lblOpcoes);
+            panel1.Location = new Point(-2, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(804, 100);
+            panel1.TabIndex = 6;
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(139)))), ((int)(((byte)(172)))), ((int)(((byte)(255)))));
-            this.panel2.Location = new System.Drawing.Point(-2, 356);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(804, 100);
-            this.panel2.TabIndex = 7;
+            panel2.BackColor = Color.FromArgb(139, 172, 255);
+            panel2.Location = new Point(-2, 356);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(804, 100);
+            panel2.TabIndex = 7;
+            // 
+            // painelMeio
+            // 
+            painelMeio.Controls.Add(button5);
+            painelMeio.Controls.Add(button4);
+            painelMeio.Controls.Add(button2);
+            painelMeio.Controls.Add(button1);
+            painelMeio.Controls.Add(button3);
+            painelMeio.Location = new Point(188, 96);
+            painelMeio.Name = "painelMeio";
+            painelMeio.Size = new Size(400, 265);
+            painelMeio.TabIndex = 8;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.Location = new Point(114, 223);
+            button5.Name = "button5";
+            button5.Size = new Size(178, 39);
+            button5.TabIndex = 6;
+            button5.Text = "Cancelar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.Location = new Point(213, 157);
+            button4.Name = "button4";
+            button4.Size = new Size(178, 39);
+            button4.TabIndex = 5;
+            button4.Text = "Consultas";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(14, 157);
+            button2.Name = "button2";
+            button2.Size = new Size(178, 39);
+            button2.TabIndex = 4;
+            button2.Text = "Coleta De Exame";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(14, 10);
+            button1.Name = "button1";
+            button1.Size = new Size(178, 39);
+            button1.TabIndex = 3;
+            button1.Text = "Doação De Sangue";
+            button1.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Name = "Form2";
-            this.Text = "Form2";
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            this.ResumeLayout(false);
-
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(painelMeio);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            painelMeio.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
-
-        private Button button1;
-        private Button button2;
         private Button button3;
-        private Button button4;
-        private Button button5;
-        private Label label1;
+        private Label lblOpcoes;
         private Panel panel1;
         private Panel panel2;
+        private Panel painelMeio;
+        private Button button5;
+        private Button button4;
+        private Button button2;
+        private Button button1;
     }
 }
--- pi-Serasa-Projeto_Saude-lnwk/Form2.cs
+++ pi-Serasa-Projeto_Saude-lnwk/Form2.cs
@@ -16,5 +16,20 @@ namespace pi_Serasa_Projeto_Saude_lnwk
         {
             InitializeComponent();
         }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+
+            panel1.Size = new Size(ClientSize.Width, 100);
+
+            panel2.Width = Width;
+            panel2.Location = new System.Drawing.Point(0, Height - panel2.Height);
+
+            painelMeio.Location = new Point(ClientSize.Width / 2 - painelMeio.Size.Width / 2, ClientSize.Height / 2 - painelMeio.Size.Height / 2);
+
+            lblOpcoes.Location = new Point(ClientSize.Width / 2 - lblOpcoes.Size.Width / 2);
+
+        }
     }
 }
