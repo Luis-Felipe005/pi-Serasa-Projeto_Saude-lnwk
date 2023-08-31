777f2ca010542c6e3d773855d19a3a8d5124fd15
williamDresch016 <135863277+williamDresch016@users.noreply.github.com>
2023-08-31 17:22:20 -0300

    Centralizacao quase pronta

---
+++ pi-Serasa-Projeto_Saude-lnwk/Consulta.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace pi_Serasa_Projeto_Saude_lnwk
+{
+    partial class Consulta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Consulta));
+            panel1 = new Panel();
+            label1 = new Label();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            panel1.SuspendLayout();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.InactiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(8, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(780, 85);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(332, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(108, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Consultas";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.InactiveCaption;
+            panel2.Location = new Point(8, 269);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(780, 85);
+            panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(button1);
+            panel3.Location = new Point(239, 93);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(296, 180);
+            panel3.TabIndex = 2;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.Location = new Point(101, 134);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 36);
+            button2.TabIndex = 1;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.Location = new Point(58, 56);
+            button1.Name = "button1";
+            button1.Size = new Size(196, 34);
+            button1.TabIndex = 0;
+            button1.Text = "Realizar Consulta";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 103);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(221, 160);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(541, 103);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(247, 160);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 4;
+            pictureBox2.TabStop = false;
+            // 
+            // Consulta
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Consulta";
+            Text = "Consulta";
+            Load += Consulta_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label1;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+    }
+}
---
+++ pi-Serasa-Projeto_Saude-lnwk/Consulta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pi_Serasa_Projeto_Saude_lnwk
+{
+    public partial class Consulta : Form
+    {
+        public Consulta()
+        {
+            InitializeComponent();
+        }
+
+        private void Consulta_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+
+
+            panel1.Size = new Size(ClientSize.Width, 100); 
+
+            label1.Location = new Point(ClientSize.Width / 2 - label1.Size.Width / 2);
+
+            panel2.Size = new Size(ClientSize.Height, 100);
+
+            panel2.Width = Width;
+            panel2.Location = new System.Drawing.Point(0, Height - panel2.Height);
+
+            panel3.Location = new Point(ClientSize.Width / 2 - panel3.Size.Width / 2, ClientSize.Height / 2 - panel3.Size.Height / 2);
+
+            int offsetX = 300; // Ajuste este valor conforme necessário
+            pictureBox1.Location = new Point(offsetX, ClientSize.Height / 2 - pictureBox1.Size.Height / 1 + 50);
+
+            //int centralizar = 500; // Ajuste este valor conforme necessário
+            //pictureBox2.Location = new Point(offsetX, ClientSize.Height /  - pictureBox2.Size.Height / 2 + 400);
+            
+            pictureBox2.Location = new Point(ClientSize.Width / 2 - pictureBox2.Size.Width / 5 + 150, ClientSize.Height / 2 - pictureBox2.Size.Height / 1 + 50);
+
+
+        }
+    }
+}
