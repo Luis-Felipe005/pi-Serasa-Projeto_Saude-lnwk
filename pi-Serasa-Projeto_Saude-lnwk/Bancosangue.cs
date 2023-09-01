a2251603679b081504be0aa10faa8fb8e976afe9
NatanaelDp <natanaeldepaula@hotmail.com>
2023-09-01 17:16:41 -0300

    A tela se adapta ao Notebook(talvez)

--- pi-Serasa-Projeto_Saude-lnwk/Bancosangue.cs
+++ pi-Serasa-Projeto_Saude-lnwk/Bancosangue.cs
@@ -23,6 +23,8 @@ namespace pi_Serasa_Projeto_Saude_lnwk
         public Bancosangue()
         {
             InitializeComponent();
+
+            
         }
 
         private void Form2_Load(object sender, EventArgs e)
@@ -67,31 +69,44 @@ namespace pi_Serasa_Projeto_Saude_lnwk
             WindowState = FormWindowState.Maximized;
 
 
-           
-            panel1.Size = new Size(ClientSize.Width, 100);
 
-            panel2.Location = new Point(ClientSize.Width / 2 - panel2.Size.Width / 2, ClientSize.Height / 2 - panel2.Size.Height / 2);
+            // Configurar a ancoragem dos controles para se adaptarem automaticamente ao redimensionamento
+            panel1.Dock = DockStyle.Top;
+            panel2.Size = new Size(300, 200); // Defina o tamanho desejado para o panel2
+            panel2.Location = new Point((ClientSize.Width - panel2.Width) / 2, (ClientSize.Height - panel2.Height) / 2);
+            pictureBox1.Dock = DockStyle.Bottom;
 
-            label1.Location = new Point(panel1.Width / 2 - label1.Size.Width / 2); //centralizar o label no panel
+            // Criar um TableLayoutPanel dentro do panel1
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            tableLayoutPanel.Dock = DockStyle.Fill;
+            tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100));
+            tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100));
+            panel1.Controls.Add(tableLayoutPanel);
 
-            panel2.Size = new Size(ClientSize.Height, 200);
+            // Adicione o label1 ao TableLayoutPanel e centralize-o
+            label1.AutoSize = true;
+            label1.Anchor = AnchorStyles.None;
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            tableLayoutPanel.Controls.Add(label1, 0, 0);
+
+            // Posicione as imagens (pictureBox2 e pictureBox3) conforme necessário
+            int offsetX = 500;
+            pictureBox2.Location = new Point(ClientSize.Width / 2 - pictureBox2.Width / 2 + offsetX, ClientSize.Height / 2 - pictureBox2.Height / 2 + 200);
+            pictureBox3.Location = new Point(offsetX, ClientSize.Height / 2 - pictureBox3.Height / 2 + 200);
+
+            // Adicione um manipulador de evento para SizeChanged do panel1
+            panel1.SizeChanged += Panel1_SizeChanged;
+        }
+
+        private void Panel1_SizeChanged(object sender, EventArgs e)
+        {
+            // Redimensionar o TableLayoutPanel para se ajustar automaticamente ao tamanho do panel1
+            ((TableLayoutPanel)panel1.Controls[0]).Size = panel1.Size;
 
 
-            pictureBox1.Size = new Size(ClientSize.Width, 200);
-
-            pictureBox1.Location = new Point(ClientSize.Width / 2 - pictureBox1.Size.Width / 2, ClientSize.Height - pictureBox1.Size.Height);
-
-            pictureBox2.Location = new Point(ClientSize.Width / 2 - pictureBox2.Size.Width / 2 + 500, ClientSize.Height / 2 - pictureBox2.Size.Height / 2 + 200);
-
-            
-
-            int offsetX = 500; // Ajuste este valor conforme necessário
-            pictureBox3.Location = new Point(offsetX, ClientSize.Height / 2 - pictureBox3.Size.Height / 2 + 200);
 
 
 
-           
-           
 
 
 
